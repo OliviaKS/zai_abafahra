2e8a30b5b011cdf882429c73d876114a92b6301f
Olivia <olivia.kasper@mmp.htwchur.ch>
2018-01-10 10:06:01 +0100

    Ski Rotation // Stop Button
    schmalere Skis eingebaut, Y-Korrektur hinzugefügt und Werte der Rotation angepasst (alles in allem Stämmbögli und Parallelfahrt nun deutlicher Unterschied) // Stop-Button in Echtzeitvisualisierung erscheint nach ca. 30s

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
   //VERBINDUNG ZU MUSEGERÄT AUFBAUEN
   //var muse = musedata.connect('http://127.0.1:8081');
   var muse = musedata.fake();
-  var tresh = dynamicThreshold(200,0.95);
+  var tresh = dynamicThreshold(200, 0.95);
 
   //STARTBILDSCHIRM
   var startbildschirm;
@@ -29,16 +29,12 @@
   var snow;
 
   //SKI
-  var linkerSki;
-  var linkerSki2;
-  var rechterSki;
-  var rechterSki2;
+  var linkerSki9;
+  var rechterSki9;
   var rotationLinks = 10;
   var rotationRechts = -10;
   var winkelLinkerSki = 0;
   var winkelRechterSki = 0;
-  var linkerSkiZ;
-  var rechterSkiZ;
 
   var dir_ski = 0;
   var speed_ski = 0.05;
@@ -50,20 +46,18 @@
   var y_mountains;
   var vel_mountains = 5;
   var dir_mountains;
-  //var mountains_hoehe;
-  //var sky_y = 723;
-  //var sky_acc;
-  //var sky_vel;
   var y_sky = 0;
   var dir_sky = 0;
   var vel_sky = 10;
   var sky7;
+  var stop;
 
   //BRILLE
   var goggles;
 
   //DASHBOARD
   var alphaWerte = [];
+  var SummeAlphaWerte;
   var fitnessGrenze = 0;
   var Titel_Feedback;
   var SehrEntspannt_Feedback;
@@ -72,17 +66,17 @@
   var barometer;
   var barometer_bg;
   var barometer_text;
-  //var sliderDB;
-  //var wertSliderDB;
 
-  function preload(){
+
+  function preload() {
     startbildschirm = loadImage('img/startbildschirm3.jpg');
 
-    linkerSki2 = loadImage('img/ski_zai_v3.png');
-    rechterSki2 = loadImage('img/ski_zai_v3.png');
+    linkerSki9 = loadImage('img/ski_zai_v11.png');
+    rechterSki9 = loadImage('img/ski_zai_v11.png');
     mountains = loadImage('img/mountain_bg.png');
     sky7 = loadImage('img/sky_v7.png');
     goggles = loadImage('img/goggles.png');
+    stop = loadImage('img/stop.png');
 
     Titel_Feedback = loadImage('img/abafahra_titel.png');
     SehrEntspannt_Feedback = loadImage('img/SehrEntspannt2.png');
@@ -96,7 +90,7 @@
 
   function setup() {
     //BASICS
-    createCanvas(1024,768);
+    createCanvas(1024, 768);
     frameRate(30);
     imageMode(CENTER);
     angleMode(DEGREES);
@@ -112,7 +106,7 @@
     //ENDPUNKTE SKIPISTE GENERIEREN
     for (var i = 0; i < n; i++) {
       //var x = map(i,0,n,-1400,width+1400);
-      var x = map(i,0,n,-6300,width+6300);
+      var x = map(i, 0, n, -6300, width + 6300);
       var y = height;
       var p = createVector(x, y);
       endpunkte.push(p);
@@ -128,10 +122,10 @@
     //SCHNEE
     snow = snowMachine(200);
     //center of the point force, um die Bewegunsrichtung zu setzen
-    snow.setCenter(width/2,height/2);
+    snow.setCenter(width / 2, height / 2);
 
     //set the flake min size and max size
-    snow.setFlakeSize(1.5,10);
+    snow.setFlakeSize(1.5, 10);
 
     //SKI
     scale_ski = 0.5;
@@ -141,27 +135,23 @@
     slider.position(30,60);
     slider.style('width','200px');*/
 
-    //SLIDER FÜR DASHBOARD --> keine senkrechten Slider möglich!!
-    /*sliderDB = createSlider (0,100,0,1);
-    slider.position()*/
-
   }
 
 
-  function draw(){
+  function draw() {
     if (state == 'einleitung') {
       drawEinleitung();
-    } else if (state == 'echtzeit'){
+    } else if (state == 'echtzeit') {
       drawEchtzeit();
-    } else if (state == 'dashboard'){
+    } else if (state == 'dashboard') {
       drawDashboard();
     }
   }
 
 
   //Wechsel des States
-  function mousePressed(){
-    if (state == 'einleitung'){
+  function mousePressed() {
+    if (state == 'einleitung') {
       y_sky = 384;
       dir_sky = 0;
       y_mountains = 550;
@@ -170,23 +160,24 @@
       fluchtpunkt = createVector(width / 2, height / 2);
       vel_mountains = 0;
       mountains_hoehe = 0;
-      var alphaWerte = [];
+      alphaWerte = [];
       state = 'echtzeit';
-    } else if (state == 'echtzeit'){
+    } else if (state == 'echtzeit') {
       state = 'dashboard';
-    } else if (state == 'dashboard'){
-      var alphaWerte = [];
+    } else if (state == 'dashboard') {
+      alphaWerte = [];
+      SummeAlphaWerte = 0;
       state = 'einleitung';
     }
   }
 
 
-  function drawEinleitung(){
-    image(startbildschirm,width/2,height/2);
+  function drawEinleitung() {
+    image(startbildschirm, width / 2, height / 2);
   }
 
 
-  function drawEchtzeit(){
+  function drawEchtzeit() {
     //BASICS
     background('#d7eef9');
     fill('white');
@@ -200,68 +191,63 @@
     var threshold = tresh.threshold(_alpha);
     var fitness = (_alpha - threshold);
 
-    acc.set(0,1*fitness);
+    acc.set(0, 1 * fitness);
     vel_fluchtpunkt.add(acc);
     vel_fluchtpunkt.mult(0.99);
     //Widerstand eingebaut durch mult - sobald keine acc mehr geht vel weg bis 0
     fluchtpunkt.add(vel_fluchtpunkt);
 
-    if (fluchtpunkt.y < 0){
+    if (fluchtpunkt.y < 0) {
       fluchtpunkt.y = 0;
-        vel_fluchtpunkt.set(0,0);
-        }
-        else if(fluchtpunkt.y>height-150){
-          fluchtpunkt.y = height-150;
-          vel_fluchtpunkt.set(0,0);
-        }
-
-    y_sky = map(fluchtpunkt.y,0,height-150,1100,-354);
-    image(sky7,512,y_sky);
-
-    y_mountains = map(fluchtpunkt.y,0,height-150,600,500);
-    image(mountains,512,y_mountains);
-
-    //Speichern der AlphaWerte für Dashboard
-    if (fitness >= fitnessGrenze){
-        alphaWerte.push(1);
-    } else {
-        alphaWerte.push(0);
+      vel_fluchtpunkt.set(0, 0);
+    } else if (fluchtpunkt.y > height - 150) {
+      fluchtpunkt.y = height - 150;
+      vel_fluchtpunkt.set(0, 0);
     }
 
-    //text('Max. AlphaWert ' + wertSlider,20,90);
+    y_sky = map(fluchtpunkt.y, 0, height - 150, 1100, -354);
+    image(sky7, 512, y_sky);
+
+    y_mountains = map(fluchtpunkt.y, 0, height - 150, 600, 500);
+    image(mountains, 512, y_mountains);
+
+    //Speichern der AlphaWerte für Dashboard
+    if (fitness >= fitnessGrenze) {
+      alphaWerte.push(1);
+    } else {
+      alphaWerte.push(0);
+    }
+
+    /*text('Max. AlphaWert ' + wertSlider,20,90);
     fill('purple');
     text('Alpha ' + _alpha*100,30,60);
-    //text('Dyn. Schwelle ' + threshold*100,30,90);
-    //text('Fitness ' + fitness*100,30,110);
+    text('Dyn. Schwelle ' + threshold*100,30,90);
+    text('Fitness ' + fitness*100,30,110);*/
 
 
     //HINTERGRUND SKIPISTE
-    //noch nicht elegant -- gleicher Code folgt unten in for-Schlaufe
-    var v = p5.Vector.lerp(endpunkte[0],fluchtpunkt,map(fluchtpunkt.y,0,height-150,0.5,1));
+    var v = p5.Vector.lerp(endpunkte[0], fluchtpunkt, map(fluchtpunkt.y, 0, height - 150, 0.5, 1));
     fill('#fcfcfc');
     noStroke();
-    rect(0,v.y,width,height-v.y);
+    rect(0, v.y, width, height - v.y);
 
 
     //SKI-RILLEN ZEICHNEN
-    //stroke('#dbdfe1');
-    //stroke('#e8eced');
     stroke('#edefef');
     strokeWeight(2.8);
     for (var i = 0; i < endpunkte.length; i++) {
       var aktuellerEndpunkt = endpunkte[i];
-      var v = p5.Vector.lerp(aktuellerEndpunkt,fluchtpunkt,map(fluchtpunkt.y,0,height-150,0.5,1));
-      //line(fluchtpunkt.x, fluchtpunkt.y, endpunkte[i].x, endpunkte[i].y);  
-      line(v.x,v.y,aktuellerEndpunkt.x,aktuellerEndpunkt.y);
+      var v = p5.Vector.lerp(aktuellerEndpunkt, fluchtpunkt, map(fluchtpunkt.y, 0, height - 150, 0.5, 1));
+      line(v.x, v.y, aktuellerEndpunkt.x, aktuellerEndpunkt.y);
     }
 
 
     //SCHNEEFLOCKEN
     //point force um Forwärtsgeschwindigkeit zu simulieren
     //snow.setFlakeSize(map(fluchtpunkt.y,0,height-150,5,1.5),map(fluchtpunkt.y,0,height-150,12,8));
-    var pforce = map(fluchtpunkt.y,0,height-150, 0, 1);
+    var pforce = map(fluchtpunkt.y, 0, height - 150, 0, 1);
     snow.setPointForce(pforce);
-    snow.setCenter(width/2,fluchtpunkt.y);
+    snow.setCenter(width / 2, fluchtpunkt.y);
     snow.draw();
 
     /*fill('purple');
@@ -270,76 +256,115 @@
 
     //SKI ROTATION
     push();
-    translate(467,1090);
-    rotationLinks = map(fluchtpunkt.y,0,height-150,25,0);
-    //var xCorrection = constrain(xCorrection,268,467);
-    var xCorrection = constrain(xCorrection,-199,0);
-    xCorrection = -rotationLinks*7;
-    translate(xCorrection,0);
+    translate(475, 943);
+    rotationLinks = map(fluchtpunkt.y, 0, height - 150, 40, 0);
+    var xCorrection = constrain(xCorrection, -199, 0);
+    xCorrection = -rotationLinks * 5;
+    var yCorrection = constrain(yCorrection, -100, 0);
+    yCorrection = -rotationLinks * 3;
+    translate(xCorrection, yCorrection);
     rotate(rotationLinks);
-    image(linkerSki2,0,0);
+    image(linkerSki9, 0, 0);
     pop();
 
 
     push();
-    translate(557,1090);
-    rotationRechts = map(fluchtpunkt.y,0,height-150,-25,0);
-    //var xCorrectionRechts = constrain(xCorrectionRechts,557,756);
-    var xCorrectionRechts = constrain(xCorrectionRechts,0,199);
-    xCorrectionRechts = -rotationRechts*7;
-    translate(xCorrectionRechts,0);
+    translate(549, 943);
+    rotationRechts = map(fluchtpunkt.y, 0, height - 150, -40, 0);
+    var xCorrectionRechts = constrain(xCorrectionRechts, 0, 199);
+    xCorrectionRechts = -rotationRechts * 5;
+    var yCorrectionRechts = constrain(yCorrectionRechts, -100, 0);
+    yCorrectionRechts = rotationRechts * 3;
+    translate(xCorrectionRechts, yCorrectionRechts);
     rotate(rotationRechts);
-    image(rechterSki2,0,0);
+    image(rechterSki9, 0, 0);
     pop();
 
-    
-  //SKIBRILLE
-  image(goggles,512,384);
-  /*fill('red');
-  ellipse(fluchtpunkt.x,fluchtpunkt.y,10,10);*/
+
+    //SKIBRILLE
+    image(goggles, 512, 384);
+    /*fill('red');
+    ellipse(fluchtpunkt.x,fluchtpunkt.y,10,10);*/
+
+    drawHorseshoe(55, 75);
+
+    if (alphaWerte.length >= 800) {
+      image(stop, 924, 80);
+    }
 
   }
 
 
-  function drawDashboard(){
+  function drawHorseshoe(left, top) {
+    //console.log('drawHorseshoe');
+    var horseshoe = muse.get('/muse/elements/horseshoe');
+    // console.log(horseshoe);
+
+    var vals = [horseshoe.leftEar, horseshoe.leftFront, horseshoe.rightFront, horseshoe.rightEar];
+    var gap = 20;
+    var d = 10;
+    push();
+    translate(left, top);
+
+    //leftEar
+    for (var i = 0; i < vals.length; i++) {
+      var x = i * gap
+      var sensorVal = vals[i];
+      var col = getColor(sensorVal);
+      noStroke();
+      fill(col);
+      ellipse(x, 0, d, d);
+    }
+
+    pop();
+
+  }
+
+
+  function getColor(val) {
+    if (val == 1) {
+      return 'green';
+    } else if (val == 2) {
+      return 'orange';
+    } else if (val >= 3) {
+      return 'red';
+    } else return 'black';
+  }
+
+
+  function drawDashboard() {
     //BASICS
     background('#d7eef9');
     fill('white');
 
-    //SLIDER
-    //wertSlider = slider.value();
-
     //BERECHNUNG DES DURCHSCHNITTLICHEN RUNTERFAHREN-ZUSTANDS
     var SummeAlphaWerte = 0;
-    for(var i=0, n=alphaWerte.length; i < n; i++){
-      //var SummeAlphaWerte = SummeAlphaWerte + alphaWerte[i];
+    for (var i = 0, n = alphaWerte.length; i < n; i++) {
       SummeAlphaWerte = SummeAlphaWerte + alphaWerte[i];
     }
 
-    var MaxEntspannt = alphaWerte.length*1;
-    var SehrEntspannt = (alphaWerte.length*1) * 0.8;
-    var MittelEntspannt = (alphaWerte.length*1) * 0.4;
+    var MaxEntspannt = alphaWerte.length * 1;
+    var SehrEntspannt = (alphaWerte.length * 1) * 0.85;
+    var MittelEntspannt = (alphaWerte.length * 1) * 0.4;
 
-    fluchtpunkt = createVector(width/2, map(SummeAlphaWerte,0,MaxEntspannt,0,height-150));
+    fluchtpunkt = createVector(width / 2, map(SummeAlphaWerte, 0, MaxEntspannt, 0, height - 150));
 
-    if (SummeAlphaWerte >= SehrEntspannt){
-      image(sky7,512,-354);
-      image(mountains,512,500);
-    }
-    else if (SummeAlphaWerte >= MittelEntspannt && SummeAlphaWerte < SehrEntspannt){
-      image(sky7,512,727);
-      image(mountains,512,550);
-    }
-    else{
-      image(sky7,512,1100);
-      image(mountains,512,600);
+    if (SummeAlphaWerte >= SehrEntspannt) {
+      image(sky7, 512, -354);
+      image(mountains, 512, 500);
+    } else if (SummeAlphaWerte >= MittelEntspannt && SummeAlphaWerte < SehrEntspannt) {
+      image(sky7, 512, 727);
+      image(mountains, 512, 550);
+    } else {
+      image(sky7, 512, 1100);
+      image(mountains, 512, 600);
     }
 
     //HINTERGRUND SKIPISTE
-    var v = p5.Vector.lerp(endpunkte[0],fluchtpunkt,map(fluchtpunkt.y,0,height-150,0.5,1));
+    var v = p5.Vector.lerp(endpunkte[0], fluchtpunkt, map(fluchtpunkt.y, 0, height - 150, 0.5, 1));
     fill('#fcfcfc');
     noStroke();
-    rect(0,v.y,width,height-v.y);
+    rect(0, v.y, width, height - v.y);
 
 
     //SKI-RILLEN ZEICHNEN
@@ -347,91 +372,89 @@
     strokeWeight(2.8);
     for (var i = 0; i < endpunkte.length; i++) {
       var aktuellerEndpunkt = endpunkte[i];
-      var v = p5.Vector.lerp(aktuellerEndpunkt,fluchtpunkt,map(fluchtpunkt.y,0,height-150,0.5,1));
-      line(v.x,v.y,aktuellerEndpunkt.x,aktuellerEndpunkt.y);
+      var v = p5.Vector.lerp(aktuellerEndpunkt, fluchtpunkt, map(fluchtpunkt.y, 0, height - 150, 0.5, 1));
+      line(v.x, v.y, aktuellerEndpunkt.x, aktuellerEndpunkt.y);
     }
 
     //DURCHSCHNITTSWERT SKIPISTE - HORIZONT LINIE
     stroke('#d89a9a');
-    line(0,v.y,width,v.y);
+    line(0, v.y, width, v.y);
 
 
     //SCHNEEFLOCKEN
-    var pforce = map(fluchtpunkt.y,0,height-150, 0, 1);
+    var pforce = map(fluchtpunkt.y, 0, height - 150, 0, 1);
     snow.setPointForce(pforce);
-    snow.setCenter(width/2,fluchtpunkt.y);
+    snow.setCenter(width / 2, fluchtpunkt.y);
     snow.draw();
 
     //FEEDBACK AN USER
-    image(Titel_Feedback,width/2, 100);
-    if (SummeAlphaWerte >= SehrEntspannt){
-      image(SehrEntspannt_Feedback,512,140);
-    }
-    else if (SummeAlphaWerte >= MittelEntspannt && SummeAlphaWerte < SehrEntspannt){
-      image(MittelEntspannt_Feedback,512,150);
-    }
-    else{
-      image(Unentspannt_Feedback,512,140);
+    image(Titel_Feedback, width / 2, 100);
+    if (SummeAlphaWerte >= SehrEntspannt) {
+      image(SehrEntspannt_Feedback, 512, 140);
+    } else if (SummeAlphaWerte >= MittelEntspannt && SummeAlphaWerte < SehrEntspannt) {
+      image(MittelEntspannt_Feedback, 512, 150);
+    } else {
+      image(Unentspannt_Feedback, 512, 140);
     }
 
     //BAROMETER
-    image(barometer_bg,970,height/2);
+    image(barometer_bg, 970, height / 2);
     strokeWeight(1);
     //stroke('#5d5f5e');
     noStroke();
     fill('#dadada');
-    rect(957,378,10,240,20);
+    rect(957, 378, 10, 240, 20);
     //image(barometer,975,400);
     /*stroke('#575756');
     fill('#575756');*/
     fill('#575756');
     textSize(12);
-    text('0%',972,388);
-    text('100%',972,618);
+    text('0%', 972, 388);
+    text('100%', 972, 618);
     textSize(12);
-   // text('Entspannigsbarometer',895,360);
-    image(barometer_text,957,360);
+    //text('Entspannigsbarometer',895,360);
+    image(barometer_text, 957, 360);
     stroke('#d89a9a');
     fill('#d89a9a');
-    ellipse(962,v.y,10,10);
+    ellipse(962, v.y, 10, 10);
 
     //BERECHNUNG ENTSPANNUNGSWERT
-    var UserEntspannt = round(SummeAlphaWerte/alphaWerte.length*100);
-    //var UserEntspanntRnd = math.round(UserEntspannt);
+    var UserEntspannt = round(SummeAlphaWerte / alphaWerte.length * 100);
     noStroke();
-    text(UserEntspannt + '%',919,v.y);
+    text(UserEntspannt + '%', 919, v.y);
 
 
     //SKI ROTATION
     push();
-    translate(467,1090);
-    rotationLinks = map(fluchtpunkt.y,0,height-150,25,0);
-    //var xCorrection = constrain(xCorrection,268,467);
-    var xCorrection = constrain(xCorrection,-199,0);
-    xCorrection = -rotationLinks*7;
-    translate(xCorrection,0);
+    translate(475, 943);
+    rotationLinks = map(fluchtpunkt.y, 0, height - 150, 40, 0);
+    var xCorrection = constrain(xCorrection, -199, 0);
+    xCorrection = -rotationLinks * 5;
+    var yCorrection = constrain(yCorrection, -100, 0);
+    yCorrection = -rotationLinks * 3;
+    translate(xCorrection, yCorrection);
     rotate(rotationLinks);
-    image(linkerSki2,0,0);
+    image(linkerSki9, 0, 0);
     pop();
 
 
     push();
-    translate(557,1090);
-    rotationRechts = map(fluchtpunkt.y,0,height-150,-25,0);
-    //var xCorrectionRechts = constrain(xCorrectionRechts,557,756);
-    var xCorrectionRechts = constrain(xCorrectionRechts,0,199);
-    xCorrectionRechts = -rotationRechts*7;
-    translate(xCorrectionRechts,0);
+    translate(549, 943);
+    rotationRechts = map(fluchtpunkt.y, 0, height - 150, -40, 0);
+    var xCorrectionRechts = constrain(xCorrectionRechts, 0, 199);
+    xCorrectionRechts = -rotationRechts * 5;
+    var yCorrectionRechts = constrain(yCorrectionRechts, -100, 0);
+    yCorrectionRechts = rotationRechts * 3;
+    translate(xCorrectionRechts, yCorrectionRechts);
     rotate(rotationRechts);
-    image(rechterSki2,0,0);
+    image(rechterSki9, 0, 0);
     pop();
 
-    
-  //SKIBRILLE
-  image(goggles,512,384);
-  fill('red');
-  //console.log('Summe AlphaWerte ' + SummeAlphaWerte + ' / ' + 'Anz. Werte ' + SummeAlphaWerte.length);
-  console.log('Summe AlphaWerte ' + SummeAlphaWerte + ' / ' + 'Anz. Werte ' + alphaWerte.length);
-  //ellipse(fluchtpunkt.x,fluchtpunkt.y,10,10);
+
+    //SKIBRILLE
+    image(goggles, 512, 384);
+    fill('red');
+    console.log('Summe AlphaWerte ' + SummeAlphaWerte + ' / ' + 'Anz. Werte ' + alphaWerte.length);
+    //ellipse(fluchtpunkt.x,fluchtpunkt.y,10,10);
 
   }
